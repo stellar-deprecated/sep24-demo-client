cfe08c9a7667879c268dad28a504eab50a9aa940
Michael Feldstein <msfeldstein@gmail.com>
2019-08-06 10:44:08 -0700

    Add missing files

---
+++ src/steps/wait_for_begin.js
@@ -0,0 +1,10 @@
+const Config = require("../config");
+
+module.exports = {
+  instruction:
+    "The flow is kicked off when someone has a credit backed by an Anchor supporting SEP-0006.  A withdraw cash button will appear near their balance.",
+  action: "Open the app",
+  execute: async function(state, { log, instruction, waitForPageContinue }) {
+    return waitForPageContinue("pages/wallet.html");
+  }
+};
