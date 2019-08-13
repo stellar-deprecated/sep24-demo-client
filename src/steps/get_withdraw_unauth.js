3ce0329fb216fffdf8dd17d7e4c0a22807927c52
Michael Feldstein <msfeldstein@gmail.com>
2019-08-13 12:21:45 -0700

    Undo change

--- src/steps/get_withdraw_unauth.js
+++ src/steps/get_withdraw_unauth.js
@@ -11,7 +11,7 @@ module.exports = {
     const USER_SK = Config.get("USER_SK");
     const pk = StellarSDK.Keypair.fromSecret(USER_SK).publicKey();
     const BRIDGE_URL = Config.get("BRIDGE_URL");
-    const withdrawType = "bank_account";
+    const withdrawType = "cash";
     const params = {
       type: withdrawType,
       asset_code: ASSET_CODE,
