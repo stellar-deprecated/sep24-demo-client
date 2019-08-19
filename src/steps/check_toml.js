baadd85eb64e60787038df8901ebd8d489e04dcb
Michael Feldstein <msfeldstein@gmail.com>
2019-08-19 11:12:50 -0700

    Allow overriding of transfer server and auth server

--- src/steps/check_toml.js
+++ src/steps/check_toml.js
@@ -26,5 +26,21 @@ module.exports = {
     );
     state.auth_endpoint = information.WEB_AUTH_ENDPOINT;
     state.transfer_server = information.TRANSFER_SERVER;
+
+    if (Config.get("WEB_AUTH_ENDPOINT")) {
+      instruction(
+        "Overriding TOMLs WEB_AUTH_ENDPOINT via config: " +
+          Config.get("WEB_AUTH_ENDPOINT"),
+      );
+      state.auth_endpoint = Config.get("WEB_AUTH_ENDPOINT");
+    }
+
+    if (Config.get("TRANSFER_SERVER")) {
+      instruction(
+        "Overriding TOMLs TRANSFER_SERVER via config: " +
+          Config.get("TRANSFER_SERVER"),
+      );
+      state.transfer_server = Config.get("TRANSFER_SERVER");
+    }
   },
 };
