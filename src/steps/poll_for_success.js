9d1964f24d384c2938113e74f46475d80c2c46a5
Michael Feldstein <msfeldstein@gmail.com>
2019-08-19 10:33:51 -0700

    Pass jwt to withdraw/transaction/deposit endpoints

--- src/steps/poll_for_success.js
+++ src/steps/poll_for_success.js
@@ -19,6 +19,11 @@ module.exports = {
         const transactionResult = await get(
           `${BRIDGE_URL}/transaction`,
           transactionParams,
+          {
+            headers: {
+              Authorization: `Bearer ${state.token}`,
+            },
+          },
         );
         response("GET /transaction", transactionResult);
         if (transactionResult.transaction.status === "completed") {
