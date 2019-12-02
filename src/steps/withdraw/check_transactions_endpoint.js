7a1c2ccdd16cfd5cd45d29eab65e6562558af175
Michael Feldstein <msfeldstein@gmail.com>
2019-12-02 15:48:39 -0800

    Wrap the transacitons test in a try/catch so it doesn't break the flow

--- src/steps/withdraw/check_transactions_endpoint.js
+++ src/steps/withdraw/check_transactions_endpoint.js
@@ -11,32 +11,36 @@ module.exports = {
     const USER_SK = Config.get("USER_SK");
     const pk = StellarSDK.Keypair.fromSecret(USER_SK).publicKey();
     const transfer_server = state.transfer_server;
-    const url = `${transfer_server}/transactions?asset_code=${ASSET_CODE}&account=${pk}&no_older_than=${state.begin_time}`;
+    const url = `${transfer_server}/transactions?asset_code=${ASSET_CODE}&no_older_than=${state.begin_time}`;
     request("GET " + url);
-    const transactionResponse = await fetch(url, {
-      headers: {
-        Authorization: `Bearer ${state.token}`,
-      },
-    });
-    expect(
-      transactionResponse.status === 200,
-      `/transactions responded with status code ${transactionResponse.status}`,
-    );
-    const transactionsResult = await transactionResponse.json();
-    response("GET /transactions", transactionsResult);
-    expect(
-      !transactionsResult.error,
-      `Transactions list had error: ${transactionsResult.error}`,
-    );
-    expect(
-      transactionsResult.transactions &&
-        transactionsResult.transactions.length > 0,
-      "There are no transactions returned, there should be at least the one we just created",
-    );
-    expect(
-      transactionsResult.transactions &&
-        transactionsResult.transactions.length < 2,
-      "There should be 1 and only 1 transaction available since this transaction started.  Perhaps the `no_older_than` flag isn't being respected.",
-    );
+    try {
+      const transactionResponse = await fetch(url, {
+        headers: {
+          Authorization: `Bearer ${state.token}`,
+        },
+      });
+      expect(
+        transactionResponse.status === 200,
+        `/transactions responded with status code ${transactionResponse.status}`,
+      );
+      const transactionsResult = await transactionResponse.json();
+      response("GET /transactions", transactionsResult);
+      expect(
+        !transactionsResult.error,
+        `Transactions list had error: ${transactionsResult.error}`,
+      );
+      expect(
+        transactionsResult.transactions &&
+          transactionsResult.transactions.length > 0,
+        "There are no transactions returned, there should be at least the one we just created",
+      );
+      expect(
+        transactionsResult.transactions &&
+          transactionsResult.transactions.length < 2,
+        "There should be 1 and only 1 transaction available since this transaction started.  Perhaps the `no_older_than` flag isn't being respected.",
+      );
+    } catch (e) {
+      expect(false, "Something went wrong fetching /transactions");
+    }
   },
 };
